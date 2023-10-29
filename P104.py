27af879089aed568979cb9f667d5a93f10ebd971
VihaansCoding <100486249+VihaansCoding@users.noreply.github.com>
2023-10-29 17:00:19 -0400

    Add files via upload

---
+++ P104.py
@@ -0,0 +1,13 @@
+import cv2
+img=cv2.imread("solar-system.jpg")
+cv2.putText(img,"Sun",(20,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Mercury",(100,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Venus",(190,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Earth",(280,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Mars",(380,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Jupitur",(540,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Saturn",(780,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Uranus",(970,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Neptune",(1110,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.imshow("solar-system.jpg",img)
+cv2.waitKey(0)
